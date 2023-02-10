c7971b5eddf7af5cfaf096cad09125d0cb568c2c
Gustavo Serra <gufserra@gmail.com>
2023-02-10 18:15:00 -0300

    assets

--- engine.py
+++ engine.py
@@ -289,7 +289,7 @@ def engine(board, depth):
     
     global best_move
     
-    with chess.polyglot.open_reader("openings/baron30.bin") as reader:
+    with chess.polyglot.open_reader("/data/data/chess-engine/assets/openings/baron30.bin") as reader:
 
         print(reader.choice(board).move)
 
